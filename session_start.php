aa3fab072d8ee7be26fc88f94437720bd17f4da1
Tim <tim.parkerd@gmail.com>
2016-07-05 22:14:01 -0400

    fixed the redirecting by removing all echo statements

--- session_start.php
+++ session_start.php
@@ -18,25 +18,21 @@ try {
     $dbh = new PDO('mysql:host=sdickerson.ddns.net;port=3306;dbname=test', 'root', 'S#8roN*PJTMQWJ4m');
     $con = new PDO('mysql:host='.$host.';dbname='.$db.';port=3306', $user, $pass);
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully<br>";
 }
 
 catch(PDOException $e)
 {
-    echo "Connection failed." . $e->getMessage();
 }
 
 
 session_start();
 if(isset($_SESSION['id']) && $_SESSION['id'] != '')
 {
-  echo  $_SESSION['id'] . ' is already logged in<br>';
+  header('location: index.php');
 } else {
 
-  echo 'Looking for POST variables<br>';
   if (isset($_POST)) {
 
-    var_dump($_POST);
     try {
       $sql = "SELECT sid FROM student WHERE email = :email AND pword = :pword";
       $stmt = $pdo->prepare($sql);
@@ -44,9 +40,6 @@ if(isset($_SESSION['id']) && $_SESSION['id'] != '')
       $stmt->bindParam(':email', $_POST["username"], PDO::PARAM_STR);
       $stmt->bindParam(':pword', $_POST["password"], PDO::PARAM_STR);
 
-      echo 'Found them:<br>';
-      echo $_POST["username"] . "<br>";
-      echo $_POST["password"] . "<br>";
 
       // Execute Query
       $stmt->execute();
@@ -68,34 +61,14 @@ if(isset($_SESSION['id']) && $_SESSION['id'] != '')
     }
 
     if ( !empty($errors)) {
-
             // if there are items in our errors array, return those errors
             $data['success'] = false;
             $data['errors']  = $errors;
-
-            // TODO(timp): Replace this with a general error message so that the user doesn't see what went wront with the database
             $data['message'] = $errors['message'];
         } else {
-
-            // if there are no errors process our form, then return a message
-
-            // DO ALL YOUR FORM PROCESSING HERE
-            // THIS CAN BE WHATEVER YOU WANT TO DO (LOGIN, SAVE, UPDATE, WHATEVER)
-
-            // show a message of success and provide a true success variable
-            // $data['success'] = true;
-            // $data['message'] = 'Successfully logged in!';
-            // $data['$_POST'] = $_POST;
-            // $data['$_SESSION'] = $_SESSION;
-            // $data['current_user_id'] = $_SESSION['id'];
             $_SESSION['id'] = $user_id;
-            echo $_SESSION['id'] . ' has logged in.';
         }
-
-      // Return messages to the caller
-      // echo json_encode($data);
     }
 }
-
 header('location: index.php');
 ?>
--- session_stop.php
+++ session_stop.php
@@ -1,13 +1,7 @@
 <?php
-
   session_start();
   if (isset($_SESSION['id']) && $_SESSION['id'] != '') {
-    var_dump($_POST);
-    var_dump($_SESSION);
-    echo $_SESSION['id'] . ' has logged out';
     session_destroy();
   }
-
   header('location: index.php');
-
  ?>
